229f284245d56dd86b1618aff6b238d2752e88c9
Kishore-1 <madhavaa.kishore@gmail.com>
2024-10-01 16:47:16 +0530

    list_cd

--- Scrap/shell_scrap.py
+++ Scrap/shell_scrap.py
@@ -6,6 +6,28 @@ import datetime
 
 import socket
 
+def list_cd():   
+# list directories
+    file_cd = []
+    a_lst = os.listdir()    # gets items in current directory
 
-# Shell Loop
+#Loop
+    for item in a_lst:
+        if os.path.isfile(item):
+            file_cd.append(item)
+
+    if file_cd:
+        for item in file_cd:
+            print(item)
+    else:
+        print("No files exists in the current directory")
+
+def shell():
+    while (1==1):
+        command = input("Kishore_Shell> ")
+
+        if command == "list":
+            list_cd()
+        else:
+            print("Invalid command")
 
