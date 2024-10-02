aff154183d3ce2f705a4b52cba158da967a4fd5b
Kishore-1 <madhavaa.kishore@gmail.com>
2024-10-02 14:36:33 +0530

    cat <filenaem>

--- src/scrap.py
+++ src/scrap.py
@@ -6,12 +6,13 @@ def content(filename):
         print(f1.read())
         f1.close()
 
-# content("film.txt")
+# content("File1")
 # content(r"C:\Users\kimuruga\Desktop\pro_shell\film.txt")
 
 command = input().strip()
 # print(command[0:3],command[3:])
 if command[:3] == "cat":
-    print ("file found")
     filename = command[4:]
     content(filename)
+else:
+    print("invalid file Name")
